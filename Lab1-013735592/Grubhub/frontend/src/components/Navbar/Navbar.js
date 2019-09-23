2f360c593dd1bc505cbfdf32b90e628b1a4f3b95
Tina Aggarwal <tina.14a@gmail.com>
2019-09-23 02:18:10 -0700

    Navbar update and Title change index.html

--- Lab1-013735592/Grubhub/frontend/src/components/Navbar/Navbar.js
+++ Lab1-013735592/Grubhub/frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import cookie from 'react-cookies';
 import { Redirect } from 'react-router';
 
@@ -19,29 +18,30 @@ class Navbar extends Component {
     if (cookie.load('cookie')) {
       console.log("Able to read cookie");
       navLogin = (
-        <ul className="nav navbar-nav navbar-right">
-          <li><Link to="/" onClick={this.handleLogout}><span className="glyphicon glyphicon-user"></span>Logout</Link></li>
-        </ul>
+        <li className="nav-item">
+          <a className="nav-link" href="/" onClick={this.handleLogout}>
+            <span className="glyphicon glyphicon-user"></span>
+            Logout
+          </a>
+        </li>
       );
     } else {
       //Else display login button
       console.log("Not Able to read cookie");
       navLogin = (
-        <ul className="navbar-nav mr-auto">
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Login
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Login
                 </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="/clientLogin">Client Login</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="/ownerLogin">Owner Login</a>
-                </div>
-              </li>
-            </ul>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="/clientLogin">Client Login</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="/ownerLogin">Owner Login</a>
+          </div>
+        </li>
       )
     }
-    
+
     // let redirectVar = null;
     // if (!cookie.load('cookie')) {
     //   redirectVar = <Redirect to="/" />
@@ -50,15 +50,20 @@ class Navbar extends Component {
     return (
       <div>
         {/* {redirectVar} */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">GRUBUHB</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">GRUBUHB</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {navLogin}
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              {navLogin}
+            </ul>
           </div>
-      </nav>
+        </nav>
       </div>
     )
   }
