60ef773c71bcffdd85d241da1dfb9d0584f20a8c
Tina Aggarwal <tina.14a@gmail.com>
2019-09-30 11:31:44 -0700

    Cancel button in upcoming orders for owner

--- Lab1-013735592/Grubhub/backend/index.js
+++ Lab1-013735592/Grubhub/backend/index.js
@@ -958,7 +958,7 @@ app.post('/submitOrder', function (req, res) {
 app.get('/upcomingOrdersForClient', function (req, res) {
     console.log("Inside get client's upcoming orders Request Handler");
 
-    var sql = "SELECT order_id, client_email, client_address, order_bill, order_table.r_id, rest_name, status from order_table, owner_profile where client_email = " + mysql.escape(sessionResponse[0].client_email) + " and status!='Delivered' and order_table.r_id = owner_profile.r_id "
+    var sql = "SELECT order_id, client_email, client_address, order_bill, order_table.r_id, rest_name, status from order_table, owner_profile where client_email = " + mysql.escape(sessionResponse[0].client_email) + " and status not in ('Delivered', 'Cancelled') and order_table.r_id = owner_profile.r_id "
     console.log(sql);
     pool.getConnection(function (err, pool) {
         if (err) {
@@ -990,7 +990,7 @@ app.get('/upcomingOrdersForClient', function (req, res) {
 app.get('/pastOrdersForClient', function (req, res) {
     console.log("Inside get client's upcoming orders Request Handler");
 
-    var sql = "SELECT order_id, client_email, client_address, order_bill, order_table.r_id, rest_name, status from order_table, owner_profile where client_email = " + mysql.escape(sessionResponse[0].client_email) + " and status='Delivered' and order_table.r_id = owner_profile.r_id "
+    var sql = "SELECT order_id, client_email, client_address, order_bill, order_table.r_id, rest_name, status from order_table, owner_profile where client_email = " + mysql.escape(sessionResponse[0].client_email) + " and status=('Delivered' or 'Cancelled') and order_table.r_id = owner_profile.r_id order by order_id desc"
     console.log(sql);
     pool.getConnection(function (err, pool) {
         if (err) {
@@ -1022,7 +1022,7 @@ app.get('/pastOrdersForClient', function (req, res) {
 app.get('/upcomingOrdersForOwner', function (req, res) {
     console.log("Inside get owner's upcoming orders Request Handler");
 
-    var sql = "SELECT * from order_table where r_id = " + sessionResponse[0].r_id + " and status!='Delivered'";
+    var sql = "SELECT * from order_table where r_id = " + sessionResponse[0].r_id + " and status  not in ('Delivered', 'Cancelled')";
     console.log(sql);
     pool.getConnection(function (err, pool) {
         if (err) {
@@ -1054,7 +1054,7 @@ app.get('/upcomingOrdersForOwner', function (req, res) {
 app.get('/pastOrdersForOwner', function (req, res) {
     console.log("Inside get owner's past orders Request Handler");
 
-    var sql = "SELECT * from order_table where r_id = " + sessionResponse[0].r_id + " and status='Delivered'";
+    var sql = "SELECT * from order_table where r_id = " + sessionResponse[0].r_id + " and status=('Delivered' or 'Cancelled') order by order_id desc";
     console.log(sql);
     pool.getConnection(function (err, pool) {
         if (err) {
@@ -1131,7 +1131,6 @@ app.post('/updateOrderStatus', function (req, res) {
         }
     });
 });
-
 
 //start your server on port 3001
 app.listen(3001);
