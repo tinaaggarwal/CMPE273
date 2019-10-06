2cfa89521cbe0c6bf777552a3c14f911c8594c8f
Tina Aggarwal <tina.14a@gmail.com>
2019-10-06 12:25:57 -0500

    AWS integration

--- Lab1-013735592/Calculator/Backend/index.js
+++ Lab1-013735592/Calculator/Backend/index.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 //app.set('view engine', 'ejs');
 
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://52.26.41.109:3000', credentials: true }));
 
 //use express session to maintain session data
 // app.use(session({
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 //Allow Access Control
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://52.26.41.109:3000');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
