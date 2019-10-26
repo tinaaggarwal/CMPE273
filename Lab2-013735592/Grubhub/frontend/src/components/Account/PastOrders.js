3f9141803c8be6b4588f4f26888574a0580fe00d
Tina Aggarwal <tina.14a@gmail.com>
2019-10-26 14:04:22 -0700

    Pagination on client upcoming and past orders

--- Lab2-013735592/Grubhub/frontend/src/components/Account/PastOrders.js
+++ Lab2-013735592/Grubhub/frontend/src/components/Account/PastOrders.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Orders from './Orders';
 import { clientOrderActions } from '../../js/actions/index';
-import  { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import Pagination from "react-js-pagination";
 
 class PastOrders extends Component {
 
@@ -11,35 +12,56 @@ class PastOrders extends Component {
             orders: [],
             order_ids: [],
             order_details: [],
-            authFlag: false
+            authFlag: false,
+            itemsPerPage: 4,
+            activePage: 1
         }
+        this.handlePageChange = this.handlePageChange.bind(this);
 
     }
 
     componentDidMount() {
 
-        this.props.pastOrdersForClient().then(() => 
-        {
+        this.props.pastOrdersForClient().then(() => {
             this.props.itemsInOrders(this.props.order_ids)
         });
 
     }
-
+    handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        this.setState({
+            activePage: pageNumber
+        });
+    }
 
     render() {
+        // Logic for displaying items
+        const indexOfLastItem = this.state.activePage * this.state.itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - this.state.itemsPerPage;
+        const currentItems = this.props.orders.slice(indexOfFirstItem, indexOfLastItem);
+
         return (
             <div className="container">
                 <Orders
-                    orders={this.props.orders}
+                    orders={currentItems}
                     order_details={this.props.order_details}
                     type="Past orders" />
+                <div>
+                    <Pagination
+                        activePage={this.state.activePage}
+                        itemsCountPerPage={3}
+                        totalItemsCount={this.props.orders.length}
+                        pageRangeDisplayed={5}
+                        onChange={this.handlePageChange}
+                    />
+                </div>
             </div>
         );
     }
 }
 
 const mapStateToProps = state => {
-    return { 
+    return {
         orders: state.clientOrder.orders,
         order_ids: state.clientOrder.order_ids,
         order_details: state.clientOrder.order_details
