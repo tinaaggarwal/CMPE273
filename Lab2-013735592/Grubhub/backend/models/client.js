cfd1952efab7e8c5f9bd185669d26c42315f9d7d
Tina Aggarwal <tina.14a@gmail.com>
2019-11-01 18:50:52 -0700

    setup mongo connection and created models

---
+++ Lab2-013735592/Grubhub/backend/models/client.js
@@ -0,0 +1,76 @@
+var mongoose = require('mongoose');
+// const crypto = require('crypto');
+// const jwt = require('jsonwebtoken');
+
+const Schema = mongoose.Schema;
+const clientSchema = new Schema({
+
+    first_name :{
+        type : String
+    },
+    last_name :{
+        type : String
+    },
+    client_email :{
+        type : String
+    },
+    password :{
+        type: String
+    },
+    street_address :{
+        type : String
+    },
+    apt :{
+        type: String
+    },
+    city :{
+        type : String
+    },
+    state :{
+        type: String
+    },
+    zip_code :{
+        type : String
+    },
+    phone :{
+        type: String
+    },
+    cross_street :{
+        type : String
+    },
+    delivery_instructions :{
+        type: String
+    },
+    address_name :{
+        type : String
+    },
+    profile_image :{
+        type: String
+    },
+    orders :[{
+        item: [{
+            item_name : {
+                type : String
+            },
+            item_quantity : {
+                type : Number
+            },
+            item_total_price : {
+                type : Number
+            }
+        }],
+        restaurant_name :{
+            type : String
+        },
+        status :{
+            type: String
+        },
+        order_bill :{
+            type: Number
+        }
+    }]
+});
+
+const Client = mongoose.model("Client", clientSchema)
+
+module.exports =  Client;
---
+++ Lab2-013735592/Grubhub/backend/models/restaurant.js
@@ -0,0 +1,86 @@
+var mongoose = require('mongoose');
+// const crypto = require('crypto');
+// const jwt = require('jsonwebtoken');
+
+const Schema = mongoose.Schema;
+const restaurantsSchema = new Schema({
+    
+    first_name :{
+        type : String
+    },
+    last_name :{
+        type : String
+    },
+    owner_email :{
+        type : String
+    },
+    restaurant_name :{
+        type : String
+    },
+    zip_code :{
+        type : String
+    },
+    password :{
+        type: String
+    },
+    profile_image :{
+        type : String
+    },
+    phone :{
+        type : String
+    },
+    rest_image :{
+        type : String
+    },
+    cuisine :{
+        type : String
+    },
+    menu :[{
+        section_name :{
+            type : String
+        },
+        section_description :{
+            type: String
+        },
+        item: [{
+            item_name : {
+                type : String
+            },
+            item_description : {
+                type : Number
+            },
+            item_image : {
+                type : Number
+            },
+            item_price : {
+                type : Number
+            }
+        }]
+    }],
+    orders :[{
+        item: [{
+            item_name : {
+                type : String
+            },
+            item_quantity : {
+                type : Number
+            },
+            item_total_price : {
+                type : Number
+            }
+        }],
+        client_email :{
+            type : String
+        },
+        status :{
+            type: String
+        },
+        order_bill :{
+            type: Number
+        }
+    }]
+});
+
+const Restaurants = mongoose.model("Restaurants", restaurantsSchema)
+
+module.exports = Restaurants;
